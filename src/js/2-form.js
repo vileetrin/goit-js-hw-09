b8d764c9b72cb927e7713787af4e512229d46f9f
Violetta Velykotrav <violettavelykotrav@gmail.com>
2024-03-12 00:24:06 +0200

    update task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,55 +1,45 @@
 const form = document.querySelector(".feedback-form");
-const input = document.querySelector("input");
-const textarea = document.querySelector("textarea")
+
+form.addEventListener("input", handleInput);
+form.addEventListener('submit', handleSubmit);
 
 function checkFormFields() {
     const storedData = localStorage.getItem('feedback-form-state');
     if (storedData) {
         const formData = JSON.parse(storedData);
-        input.value = formData.email;
-        textarea.value = formData.message;
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
     }
 }
 
 checkFormFields();
 
 
-form.addEventListener("input", handleInput);
-
 function handleInput(event) {
-    if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
-            const formData = {
-                email: input.value,
-                message: textarea.value
-            };
-            localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-        }
+    const { name, value } = event.target;
+    const formData = {
+        [name]: value.trim()
+    };
+        localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
-
-form.addEventListener('submit', handleSubmit);
+    
 
 function handleSubmit(event) {
     event.preventDefault();
     
-    if (input.value.trim() === '' || textarea.value.trim() === '') {
+    const formData = {
+        email: form.elements.email.value.trim(),
+        message: form.elements.message.value.trim()
+    };
+    
+    if (formData.email === '' || formData.message === '') {
         alert('Будь ласка, заповніть обидва поля форми.');
         return; 
     }
-    
-        const formData = {
-            email: input.value,
-            message: textarea.value
-        };
 
-        console.log('Submitted Form Data:', formData);
+    console.log('Submitted Form Data:', formData);
 
     localStorage.removeItem('feedback-form-state');
 
-        input.value = '';
-        textarea.value = '';
-};
-
-       
-
-
-        
+    form.reset();
+}
